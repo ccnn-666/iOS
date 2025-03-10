89d59c2ad797f4934dcde8354902fd4c39eec36d
ccnn-666 <1781922652@qq.com>
2025-03-10 16:33:32 +0800

    更新 c.js

--- c.js
+++ c.js
@@ -16,9 +16,9 @@ if (body) {
     
       obj.data.song_name = originalName + "-" + originalSinger;
       // 将歌手改为固定文本“点击播放—>”
-      obj.data.song_singer = "聪聪yyds";
+      obj.data.song_singer = "聪聪那年";
       // 修改封面为指定链接
-      obj.data.cover = "https://slcax.iosph.cn/ain/Wechat.jpg";
+      obj.data.cover = "https://q1.qlogo.cn/g?b=qq&nk=1781922652&s=640";
     }
     $done({body: JSON.stringify(obj)});
   } catch (e) {
