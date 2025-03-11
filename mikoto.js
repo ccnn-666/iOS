f88bcebdc63ddb3ae94db614fb370f1274289250
ccnn-666 <1781922652@qq.com>
2025-03-12 07:27:34 +0800

    更新 mikoto.js

--- mikoto.js
+++ mikoto.js
@@ -7,9 +7,9 @@ if (body) {
       let originalName = obj.data.song_name || "";
       
       // 自定义歌手
-      let customSinger = "Cover 聪聪"; // 这里修改为你想要的自定义歌手
+      let customSinger = "（Cover 聪聪）"; // 这里修改为你想要的自定义歌手
       
-      obj.data.song_name = originalName + " - " + customSinger;
+      obj.data.song_name = originalName +  + customSinger;
       // 将歌手改为固定文本
       obj.data.song_singer = "聪聪那年";
       // 修改封面为指定链接
