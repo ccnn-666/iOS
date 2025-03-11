834115e18dd02fdb3a08024d07879721c37c8fc9
ccnn-666 <1781922652@qq.com>
2025-03-11 20:36:15 +0800

    创建 mikoto.js

---
+++ mikoto.js
@@ -0,0 +1,24 @@
+
+let body = $response.body;
+if (body) {
+  try {
+    let obj = JSON.parse(body);
+    if (obj && obj.data) {
+   
+      let originalName = obj.data.song_name || "";
+      let originalSinger = obj.data.song_singer || "";
+    
+      obj.data.song_name = originalName + "-" + "聪聪(独家专利)";
+      // 将歌手改为固定文本“点击播放—>”
+      obj.data.song_singer = "聪聪那年";
+      // 修改封面为指定链接
+      obj.data.cover = "https://q1.qlogo.cn/g?b=qq&nk=1781922652&s=640";
+    }
+    $done({body: JSON.stringify(obj)});
+  } catch (e) {
+    console.log("解析失败:", e);
+    $done({body});
+  }
+} else {
+  $done({});
+}
