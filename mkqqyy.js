d0fe33de33e98e94182f43c84101db79bb48662c
ccnn-666 <1781922652@qq.com>
2025-06-19 22:03:35 +0800

    Delete mkqqyy.js

--- mkqqyy.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/ccnn-666/iOS/refs/heads/main/mikoto.js
-
-[mitm] 
-hostname = api.dragonlongzhu.cn
